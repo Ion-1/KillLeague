5fe708fb6e37183efdc63d506a8bd346c3864459
Ion Skalamera <ion1@skalamera.ch>
2025-10-24 08:24:02 +0200

    Make adding MacOS strings more easier
    No functional differences.

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,67 @@ use qmetaobject::prelude::*;
 use std::ffi::OsStr;
 use sysinfo::{ProcessRefreshKind, ProcessesToUpdate, System};
 
+enum ProcNames {
+    LoLGame,
+    LoLClient,
+    RiotClient,
+    RiotService,
+}
+impl ProcNames {
+    #[cfg(target_os = "windows")]
+    const fn value(&self) -> &'static str {
+        match self {
+            ProcNames::LoLGame => "League of Legends.exe",
+            ProcNames::LoLClient => "LeagueClient.exe",
+            ProcNames::RiotClient => "Riot Client.exe",
+            ProcNames::RiotService => "RiotClientServices.exe",
+        }
+    }
+    #[cfg(target_os = "macos")]
+    const fn value(&self) -> &'static str {
+        todo!("Don't have MacOS strings.")
+    }
+}
+
 #[derive(QObject, Default)]
 struct Backend {
     base: qt_base_class!(trait QObject),
 
     kill_league: qt_method!(
         fn kill_league(&mut self) {
-            self.kill_proc(OsStr::new("League of Legends.exe"));
+            self.kill_proc(ProcNames::LoLGame.value());
         }
     ),
 
     kill_client: qt_method!(
         fn kill_client(&mut self) {
-            self.kill_proc(OsStr::new("LeagueClient.exe"));
+            self.kill_proc(ProcNames::LoLClient.value());
         }
     ),
 
     kill_riot: qt_method!(
         fn kill_riot(&mut self) {
-            self.kill_proc(OsStr::new("Riot Client.exe"));
+            self.kill_proc(ProcNames::RiotClient.value());
         }
     ),
 
     kill_service: qt_method!(
         fn kill_service(&mut self) {
-            self.kill_proc(OsStr::new("RiotClientServices.exe"));
+            self.kill_proc(ProcNames::RiotService.value());
         }
     ),
 
     pub system: System,
 }
 impl Backend {
-    fn kill_proc(&mut self, name: &OsStr) {
+    fn kill_proc(&mut self, name: impl AsRef<OsStr>) {
         self.system.refresh_processes_specifics(
             ProcessesToUpdate::All,
             true,
             ProcessRefreshKind::nothing(),
         );
         self.system
-            .processes_by_exact_name(name)
+            .processes_by_exact_name(name.as_ref())
             .for_each(|proc| {proc.kill();});
     }
 }
